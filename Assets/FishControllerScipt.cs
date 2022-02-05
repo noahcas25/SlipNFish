ed6952991557e95c05e96b99025913b9f0601f69
Noah <Noah_castro25@yahoo.com>
2022-02-05 06:41:48 -0800

    Initial Asset and player testing
    Player and controls set in place

---
+++ Assets/FishControllerScipt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishControllerScipt : MonoBehaviour
+{
+    // [SerializeField]
+    // Transform centerPoint;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.forward * Time.deltaTime * 1;
+    } 
+}
---
+++ Assets/PlayerControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+  private Rigidbody rb;
+  private Animator anim;
+
+   void Start() {
+       rb = GetComponent<Rigidbody>();
+       anim =  GetComponent<Animator>();
+   }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+    } 
+
+      private void Move() {
+       if(Input.GetKey("w")) {
+           rb.AddForce(transform.forward * 15f * Time.deltaTime , ForceMode.VelocityChange);
+           anim.Play("Running");
+       }
+       else if(Input.GetKey("s")) {
+           rb.AddForce(transform.forward * 25f * Time.deltaTime, ForceMode.VelocityChange);
+            anim.Play("sliding");
+       } else {
+           anim.Play("idle");
+       }
+
+        if(Input.GetKey("a")) {
+            transform.Rotate(0, -4, 0);
+       }
+
+        if(Input.GetKey("d")) {
+            transform.Rotate(0, 4, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Fish")) 
+            other.gameObject.SetActive(false);
+    }
+}
